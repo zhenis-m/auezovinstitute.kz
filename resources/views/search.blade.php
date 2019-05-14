9212f9457ad592358625eb09d4dc598ceadabe9b
Zhenis M <zhenis.m@bilimland.kz>
2019-05-14 17:17:50 +0600

    create new style + logo

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -1,20 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>{{ trans('search.title') }}</h1>
-    <div class="pnwes__page">
-        @foreach ($newses as $news)
-            <div class="pnwes__page__block">
-                <div class="pnwes__page__block__wrap">
-                    <a href="{{ route('news.show', [ 'id' => $news->id ]) }}"><img src="{{ $news ? asset('uploads/' . $news->image_show) : ""}}"></a>
-                    <div class="pnwes__page__block__text">
-                        <p>{{ $news->title }}<a href="{{ route('news.show', [ 'id' => $news->id ]) }}">Читать дальше</a></p>
+    <div class="search__wrap">
+        <h1>{{ trans('search.title') }}</h1>
+        <div class="pnwes__page">
+            @foreach ($newses as $news)
+                <div class="pnwes__page__block">
+                    <div class="pnwes__page__block__wrap">
+                        <a href="{{ route('news.show', [ 'id' => $news->id ]) }}"><img src="{{ $news ? asset('uploads/' . $news->image_show) : ""}}"></a>
+                        <div class="pnwes__page__block__text">
+                            <p>{{ $news->title }}<a href="{{ route('news.show', [ 'id' => $news->id ]) }}">Читать дальше</a></p>
+                        </div>
+                    </div>
+                    <div class="pnew__state__date">
+                        <i class="far fa-calendar-alt"></i><p>&nbsp;&nbsp;{{ $news->created_at }}&nbsp;&nbsp;</p><i class="far fa-eye"></i><p>&nbsp;&nbsp;1222</p>
                     </div>
                 </div>
-                <div class="pnew__state__date">
-                    <i class="far fa-calendar-alt"></i><p>&nbsp;&nbsp;{{ $news->created_at }}&nbsp;&nbsp;</p><i class="far fa-eye"></i><p>&nbsp;&nbsp;1222</p>
-                </div>
-            </div>
-        @endforeach
+            @endforeach
+        </div>
     </div>
+
 @endsection
