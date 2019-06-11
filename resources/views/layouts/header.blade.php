19b206eb949860e437cc4d1b8a829e02f840bf98
Zhenis <zhenis.m@bilimmail.kz>
2019-06-11 11:55:12 +0600

    new style header

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -4,90 +4,20 @@
             <div class="logo">
                 <a href="{{ URL::to('/') }}"><img src="../../images/logo-litart2.png"></a>
             </div>
-            <div class="nav">
-                <ul class="menu">
-                    <li class="menu-item">
-                        <a href="#" class="menu-link">Институт <i class="fas fa-chevron-down"></i></a>
-                        <ul class="sub-menu sub-menu-mod">
-                            <li>
-                                <a href="{{ URL::to('/news') }}">{{ trans('header.news') }}</a>
-                            </li>
-                            <li>
-                            @foreach ($aboutses as $abouts)
-                                <li>
-                                    <a href="{{ route('abouts.show', [ 'id' => $abouts->id ]) }}" onclick="">{{ $abouts->title }}</a>
-                                </li>
-                                @endforeach
-                                </li>
-
-
-                            <li>
-                                <a href="{{ URL::to('/structure/administration') }}">{{ trans('header.admins') }}</a>
-                            </li>
-                            <li>
-                                <a href="{{ route('departments.index') }}">{{ trans('header.departmens') }}</a>
-
-                            </li>
-                        </ul>
-                    </li>
-
-                    <li class="menu-item">
-                        <a href="#" class="menu-link">{{ trans('header.science') }} <i class="fas fa-chevron-down"></i></a>
-                        <ul class="sub-menu sub-menu2-mod">
-
-                        </ul>
-                    </li>
-
-                    <li class="menu-item">
-                        <a href="#" class="menu-link">{{ trans('header.culture') }} <i class="fas fa-chevron-down"></i></a>
-                        <ul class="sub-menu sub-menu3-mod">
-                            @foreach ($cultures as $cultur)
-                                <li>
-                                    <a href="{{ route('main.show', [ 'id' => $cultur->id ]) }}">{{ $cultur->title }}</a>
-                                </li>
-                            @endforeach
-                            <li>
-                                <a href="{{ URL::to('/books') }}">{{ trans('header.books') }}</a>
-                            </li>
-                        </ul>
-                    </li>
-                </ul>
-            </div>
         </div>
-{{--        <div class="header__lang">--}}
-{{--            @foreach(LaravelLocalization::getSupportedLocales() as $localeCode => $properties)--}}
-{{--                <li>--}}
-{{--                    <a rel="alternate" hreflang="{{ $localeCode }}" href="{{ LaravelLocalization::getLocalizedURL($localeCode, null, [], true) }}">--}}
-{{--                        {{ $properties['name'] }}--}}
-{{--                    </a>--}}
-{{--                </li>--}}
-{{--            @endforeach--}}
-{{--        </div>--}}
-{{--        <div class="btn-group" id="lang-btn">--}}
-{{--            <button type="button" data-toggle="dropdown" id="header__local" >{{ trans('main.language') }}--}}
-{{--                <span class="caret"></span>--}}
-{{--            </button>--}}
-{{--            <ul class="dropdown-menu">--}}
-{{--                @foreach(LaravelLocalization::getSupportedLocales() as $localeCode => $properties)--}}
-{{--                    <li>--}}
-{{--                        <a rel="alternate" hreflang="{{ $localeCode }}" href="{{ LaravelLocalization::getLocalizedURL($localeCode, null, [], true) }}">--}}
-{{--                            {{ $properties['name2'] }}--}}
-{{--                        </a>--}}
-{{--                    </li>--}}
-{{--                @endforeach--}}
-{{--            </ul>--}}
-{{--        </div>--}}
         <div class="header__wrap">
             <div class="header__local">
-                <a href="#box" class="lang-flag {{ app()->getLocale() }}" onclick="openbox('box'); return false">{{ trans('header.language') }}</a>
-                <div id="box" class="header__local__item" style="display: none">
-                    @foreach(LaravelLocalization::getSupportedLocales() as $localeCode => $properties)
-                        <li class="lang-flag {{ $localeCode }}" data="{{ $localeCode }}">
-                            <a rel="alternate" hreflang="{{ $localeCode }}" href="{{ LaravelLocalization::getLocalizedURL($localeCode, null, [], true) }}">
-                                {{ $properties['name2'] }}
-                            </a>
-                        </li>
-                    @endforeach
+{{--                <a href="#box" class="lang-flag {{ app()->getLocale() }}" onclick="openbox('box'); return false">{{ trans('header.language') }}</a>--}}
+                <div class="header__local__item">
+                    <ul>
+                        @foreach(LaravelLocalization::getSupportedLocales() as $localeCode => $properties)
+                            <li class="lang-flag {{ $localeCode }}" data="{{ $localeCode }}">
+                                <a rel="alternate" hreflang="{{ $localeCode }}" href="{{ LaravelLocalization::getLocalizedURL($localeCode, null, [], true) }}">
+                                    {{ $properties['name'] }}
+                                </a>
+                            </li>
+                        @endforeach
+                    </ul>
                 </div>
             </div>
             <div class="search search_clearfix">
@@ -98,6 +28,59 @@
                     <input type="image" src="../../images/search.png"/>
                 </form>
             </div>
+        </div>
+    </div>
+    <div class="header__menu">
+        <div class="nav">
+            <ul class="menu">
+                <li class="menu-item">
+                    <a href="#" class="menu-link">Институт <i class="fas fa-chevron-down"></i></a>
+                    <ul class="sub-menu sub-menu-mod">
+                        <li>
+                            <a href="{{ URL::to('/news') }}">{{ trans('header.news') }}</a>
+                        </li>
+                        @foreach ($aboutses as $abouts)
+                            <li>
+                                <a href="{{ route('abouts.show', [ 'id' => $abouts->id ]) }}" onclick="">{{ $abouts->title }}</a>
+                            </li>
+                        @endforeach
+                        <li>
+                            <a href="{{ URL::to('/structure/administration') }}">{{ trans('header.admins') }}</a>
+                        </li>
+                        <li>
+                            <a href="{{ route('departments.index') }}">{{ trans('header.departmens') }}</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="menu-item">
+                    <a href="#" class="menu-link">{{ trans('header.science') }}</a>
+                </li>
+                <li class="menu-item">
+                    <a href="#" class="menu-link">{{ trans('header.culture') }} <i class="fas fa-chevron-down"></i></a>
+                    <ul class="sub-menu sub-menu3-mod">
+                        @foreach ($cultures as $cultur)
+                            <li>
+                                <a href="{{ route('main.show', [ 'id' => $cultur->id ]) }}">{{ $cultur->title }}</a>
+                            </li>
+                        @endforeach
+                        <li>
+                            <a href="{{ URL::to('/books') }}">{{ trans('header.books') }}</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="menu-item">
+                    <a href="#" class="menu-link">{{ trans('header.university') }}</i></a>
+                </li>
+                <li class="menu-item">
+                    <a href="#" class="menu-link">{{ trans('header.keruen') }}</a>
+                </li>
+                <li class="menu-item">
+                    <a href="#" class="menu-link">{{ trans('header.gold') }}</a>
+                </li>
+                <li class="menu-item">
+                    <a href="#" class="menu-link">{{ trans('header.museum') }}</a>
+                </li>
+            </ul>
         </div>
     </div>
 </div>
@@ -130,18 +113,18 @@
         }
     }
 
-    function openbox(box) {
-        var display = document.getElementById('box').style.display;
-        if (display == "none") {
-            document.getElementById('box').style.display = "block";
-        } else {
-            document.getElementById('box').style.display = "none";
-        }
-    }
-
-    window.onscroll = function() {
-        document.getElementById('box').style.display = "none";
-    }
+    // function openbox(box) {
+    //     var display = document.getElementById('box').style.display;
+    //     if (display == "none") {
+    //         document.getElementById('box').style.display = "block";
+    //     } else {
+    //         document.getElementById('box').style.display = "none";
+    //     }
+    // }
+    //
+    // window.onscroll = function() {
+    //     document.getElementById('box').style.display = "none";
+    // }
 
 
 </script>
