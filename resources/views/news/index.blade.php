d1cb6d3e0e417202dd3d1dc90798293a9937cd2a
Zhenis <zhenis.m@bilimmail.kz>
2019-06-13 17:13:23 +0600

    page-news new style

--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -12,21 +12,23 @@
 		</div>
 		<div class="pnwes__page">
 			@foreach ($newses as $news)
-			<div class="pnwes__page__block">
-				<div class="pnwes__page__block__wrap">
-					<div class="pnwes__page__block__wrap__img">
-						<a href="{{ route('news.show', [ 'id' => $news->id ]) }}"><img src="{{ $news ? asset('uploads/' . $news->image_show) : ""}}"></a>
-					</div>
-					<div class="pnwes__page__block__text">
-						<p>{!! $news->description_short !!}</p>
-						<a href="{{ route('news.show', [ 'id' => $news->id ]) }}">{{ trans('content.read_more') }}</a>
-					</div>
-				</div>
-				<div class="pnew__state__date">
-					<i class="far fa-calendar-alt"></i><p>&nbsp;&nbsp;{{ $news->created_at }}&nbsp;&nbsp;</p>
-{{--                    <i class="far fa-eye"></i><p>&nbsp;&nbsp;1222</p>--}}
-				</div>
-			</div>
+                <a class="news__link" href="{{ route('news.show', [ 'id' => $news->id ]) }}">
+                    <div class="pnwes__page__state">
+                        <div class="pnwes__page__state__wrap">
+                            <div class="pnwes__page__state__wrap__img">
+                                <img src="{{ $news ? asset('uploads/' . $news->image_show) : ""}}">
+                            </div>
+                            <div class="pnwes__page__state__text">
+                                <p>{!! $news->description_short !!}</p>
+{{--                                {{ trans('content.read_more') }}--}}
+                            </div>
+                        </div>
+                        <div class="pnew__state__date">
+                            <i class="far fa-calendar-alt"></i><p>&nbsp;&nbsp;{{ $news->created_at }}&nbsp;&nbsp;</p>
+        {{--                    <i class="far fa-eye"></i><p>&nbsp;&nbsp;1222</p>--}}
+                        </div>
+                    </div>
+                </a>
 			@endforeach
                 <tfoot>
                 <tr colspan="3">
